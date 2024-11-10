7e3bcc33fa4a5ad6167c56ad0f62404805e0f24c
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-10 23:43:14 +0400

    feat: update app module
    - add modules
    - remove obsolete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
+import { FavsModule } from './favs/favs.module';
+import { TrackModule } from './track/track.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
 })
 export class AppModule {}

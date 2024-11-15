581f5f091d26ec30a33571002e200d79603b4773
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-15 20:08:46 +0400

    refactor: track

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,119 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { randomUUID } from 'node:crypto';
-import { IdNotFoundException } from 'src/common/exceptions';
-import { Fav } from 'src/common/fav.generic.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
-import type { IAlbum } from 'src/album/interfaces/album.interface';
-import type { IArtist } from 'src/artist/interfaces/artist.interface';
-import type { ITrack } from './interfaces/track.interface';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
-export class TrackService extends Fav {
-  private readonly tracks = new Map<ITrack['id'], ITrack>();
+export class TrackService {
+  constructor(private readonly prisma: PrismaService) {}
 
-  private readonly artistTracks = new Map<IArtist['id'], Set<ITrack['id']>>();
-
-  private readonly albumTracks = new Map<IAlbum['id'], Set<ITrack['id']>>();
-
-  create(createTrackDto: CreateTrackDto) {
-    const id = randomUUID() as string;
-
-    const track = { ...createTrackDto, id };
-
-    this.tracks.set(id, track);
-
-    if (track.artistId != undefined) {
-      const artistId = track.artistId;
-
-      if (!this.artistTracks.has(artistId))
-        this.artistTracks.set(artistId, new Set());
-
-      const set = this.artistTracks.get(artistId)!;
-      set.add(track.id);
-      this.artistTracks.set(id, set);
-    }
-
-    if (track.albumId != undefined) {
-      const albumId = track.albumId;
-
-      if (!this.albumTracks.has(albumId))
-        this.albumTracks.set(albumId, new Set());
-
-      const set = this.albumTracks.get(albumId)!;
-      set.add(track.id);
-      this.albumTracks.set(id, set);
-    }
-
-    return track;
+  create(data: CreateTrackDto) {
+    return this.prisma.track.create({ data });
   }
 
-  removeArtist(artistId: string) {
-    const trackIds = this.artistTracks.get(artistId);
-
-    if (trackIds == undefined) return;
-
-    for (const trackId of trackIds) {
-      const track = this.tracks.get(trackId);
-
-      if (track == undefined) continue;
-
-      track.artistId = null;
-
-      this.tracks.set(trackId, track);
-    }
-  }
-
-  removeAlbum(albumId: string) {
-    const albumsIds = this.albumTracks.get(albumId);
-
-    if (albumsIds == undefined) return;
-
-    for (const trackId of albumsIds) {
-      const track = this.tracks.get(trackId);
-
-      if (track == undefined) continue;
-
-      track.albumId = null;
-
-      this.tracks.set(trackId, track);
-    }
-  }
-
-  hasOne(id: string) {
-    return this.tracks.has(id);
+  async hasOne(id: string) {
+    return (await this.prisma.track.count({ where: { id } })) > 0;
   }
 
   findAll() {
-    return Array.from(this.tracks.values());
+    return this.prisma.track.findMany();
   }
 
   findOne(id: string) {
-    return this.tracks.get(id);
+    return this.prisma.track.findUnique({ where: { id } });
   }
 
-  update(id: string, updateTrackDto: UpdateTrackDto) {
-    const track = this.tracks.get(id);
-
-    if (track == undefined) throw new IdNotFoundException();
-
-    const newTrack = { ...track, ...updateTrackDto, id };
-
-    this.tracks.set(id, newTrack);
-
-    return newTrack;
+  update(id: string, data: UpdateTrackDto) {
+    return this.prisma.track.update({
+      where: { id },
+      data,
+    });
   }
 
   remove(id: string) {
-    this.tracks.delete(id);
-    this.artistTracks.delete(id);
-    this.deleteFav(id);
-    this.albumTracks.delete(id);
-  }
-
-  async getFavs() {
-    const favIds = this.getFavIds();
-
-    return favIds.map((id) => this.tracks.get(id));
+    return this.prisma.track.delete({ where: { id } });
   }
 }

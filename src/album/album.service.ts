76a3606a0f8337bc80a86e10c6a9ceb913585d3d
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-15 20:08:20 +0400

    refactor: album

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,97 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
-import { randomUUID } from 'node:crypto';
 import { IAlbum } from './interfaces/album.interface';
-import { TrackService } from 'src/track/track.service';
 import { IArtist } from 'src/artist/interfaces/artist.interface';
-import { Fav } from 'src/common/fav.generic.service';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Injectable()
-export class AlbumService extends Fav {
+export class AlbumService {
   private readonly albums = new Map<IAlbum['id'], IAlbum>();
   private readonly albumsArtist = new Map<IArtist['id'], Set<IAlbum['id']>>();
 
-  constructor(private readonly trackService: TrackService) {
-    super();
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  create(createAlbumDto: CreateAlbumDto) {
-    const id = randomUUID();
-
-    const album = { ...createAlbumDto, id };
-
-    this.albums.set(id, album);
-    const artistId = album.artistId;
-
-    if (artistId == null) return album;
-
-    if (!this.albumsArtist.has(artistId)) {
-      this.albumsArtist.set(artistId, new Set());
-    }
-
-    const albumsIds = this.albumsArtist.get(artistId);
-
-    albumsIds?.add(id);
-
-    if (albumsIds != undefined) {
-      this.albumsArtist.set(artistId, albumsIds);
-    }
-
-    return album;
-  }
-
-  removeArtist(artistId: string) {
-    const albums = this.albumsArtist.get(artistId);
-
-    if (albums == undefined) return;
-
-    for (const albumId of albums) {
-      const album = this.albums.get(albumId);
-
-      if (album == undefined) continue;
-
-      album.artistId = null;
-
-      this.albums.set(album.id, album);
-    }
-
-    this.albumsArtist.delete(artistId);
+  create(data: CreateAlbumDto) {
+    return this.prisma.album.create({ data });
   }
 
   findAll() {
-    return Array.from(this.albums.values());
+    return this.prisma.album.findMany();
   }
 
   findOne(id: string) {
-    return this.albums.get(id);
+    return this.prisma.album.findUnique({ where: { id } });
   }
 
-  hasOne(id: string) {
-    return this.albums.has(id);
+  async hasOne(id: string) {
+    return (await this.prisma.album.count({ where: { id } })) > 0;
   }
 
-  update(id: string, updateAlbumDto: UpdateAlbumDto) {
-    const album = this.albums.get(id);
-
-    if (album == undefined) return;
-
-    const newAlbum = { ...album, ...updateAlbumDto, id };
-
-    this.albums.set(id, newAlbum);
-
-    return newAlbum;
+  update(id: string, data: UpdateAlbumDto) {
+    return this.prisma.album.update({ where: { id }, data });
   }
 
   remove(id: string) {
-    this.trackService.removeAlbum(id);
-    this.deleteFav(id);
-    this.albums.delete(id);
-  }
-
-  async getFavs() {
-    const favIds = this.getFavIds();
-
-    return favIds.map((id) => this.albums.get(id));
+    return this.prisma.album.delete({ where: { id } });
   }
 }

408f54939a79f1ae74984d2eece800c28834f4b5
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-22 16:04:25 +0400

    feat(logs): add exception filter with logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,19 @@ import { ArtistModule } from './artist/artist.module';
 import { FavsModule } from './favs/favs.module';
 import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerInterceptor } from './common/logger.interceptor';
+import { ExceptionFilter } from './common/exception.filter';
 
 @Module({
   providers: [
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionFilter,
     },
   ],
   imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],

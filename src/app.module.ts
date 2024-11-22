0f18f449766db671bdd6c3057ebe90053a3cfc7b
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-22 16:02:58 +0400

    feat(logs): add logger interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,16 @@ import { ArtistModule } from './artist/artist.module';
 import { FavsModule } from './favs/favs.module';
 import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './common/logger.interceptor';
 
 @Module({
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
   imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
 })
 export class AppModule {}

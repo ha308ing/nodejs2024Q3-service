58761ca71366ab78a17271c5069041a0b62de007
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-23 18:27:39 +0400

    feat(auth): add auth to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { ArtistModule } from './artist/artist.module';
 import { FavsModule } from './favs/favs.module';
 import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerInterceptor } from './common/logger.interceptor';
 import { ExceptionFilter } from './common/exception.filter';
+import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './common/auth.guard';
 
 @Module({
   providers: [
@@ -18,7 +20,18 @@ import { ExceptionFilter } from './common/exception.filter';
       provide: APP_FILTER,
       useClass: ExceptionFilter,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
-  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule,
+    AlbumModule,
+    FavsModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}

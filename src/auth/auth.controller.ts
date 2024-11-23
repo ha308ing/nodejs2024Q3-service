bf1602739fef5ee230db59c6f0cd076aa1b93866
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-23 18:25:51 +0400

    feat(auth): add auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,23 +10,27 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RefreshDto } from './dto/refresh.dto';
+import { Public } from '../common/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.CREATED)
   @Post('signup')
   signup(@Body(ValidationPipe) input: LoginDto) {
     return this.authService.signup(input);
   }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() input: LoginDto) {
     return this.authService.login(input);
   }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('refresh')
   refresh(

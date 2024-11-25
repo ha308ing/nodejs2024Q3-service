7acab73c9b57a12fd2685bc123f36c7e8628c68d
ha308ing <75647984+ha308ing@users.noreply.github.com>
2024-11-25 13:09:52 +0400

    feat(logs): keep old logs

--- src/logger/log.ts
+++ src/logger/log.ts
@@ -1,9 +1,10 @@
 import { createWriteStream, mkdirSync, WriteStream } from 'fs';
-import { CreateWriteStreamOptions, writeFile } from 'fs/promises';
+import { CreateWriteStreamOptions, rename, writeFile } from 'fs/promises';
 import { join, resolve } from 'path';
 
 const LOGS_DIR = 'logs';
 const LOG_STREAM_OPTIONS: CreateWriteStreamOptions = { encoding: 'utf-8' };
+const LOG_EXTENSION = '.log';
 
 const LOG_LIMIT_MULTIPLIER = 1024;
 const LOG_LIMIT = process.env?.LOG_LIMIT_KB;
@@ -14,12 +15,14 @@ const root = process.cwd();
 mkdirSync(resolve(root, LOGS_DIR), { recursive: true });
 
 export class Log {
+  private logName: string;
   private logFile: string;
   private logStream: WriteStream;
 
   constructor(logName = 'log') {
-    this.logFile = resolve(root, join(LOGS_DIR, logName + '.log'));
-    this.resetLogStream();
+    this.logName = logName;
+    this.logFile = this.getLogPath();
+    this.logStream = this.createLogStream();
   }
 
   public async write(message: string) {
@@ -27,23 +30,50 @@ export class Log {
       isLimit && this.logStream.bytesWritten + message.length >= logLimit;
 
     if (shouldResetLog) {
-      await this.resetLogStream();
+      await this.resetLog();
     }
 
     this.logStream.write(message);
   }
 
-  private async resetLogStream() {
-    this.logStream?.close();
-    await this.resetLogFile();
-    this.createLogStream();
-  }
-
-  private resetLogFile() {
+  private clearLogFile() {
     return writeFile(this.logFile, '', { flag: 'w+' });
   }
 
-  private createLogStream(options = LOG_STREAM_OPTIONS) {
-    this.logStream = createWriteStream(this.logFile, options);
+  private async resetLog() {
+    this.logStream.end();
+
+    const newFile = this.getNewLogPath();
+    await rename(this.logFile, newFile);
+
+    this.logStream = this.createLogStream();
+
+    await this.clearLogFile();
+  }
+
+  private createLogStream(path = this.logFile, options = LOG_STREAM_OPTIONS) {
+    return createWriteStream(path, options);
+  }
+
+  private getNewLogPath() {
+    const date = new Date();
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth();
+    const day = date.getUTCDate();
+    const hour = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getUTCSeconds();
+
+    const timeString = [year, month, day, hour, minutes, seconds]
+      .map((t) => ('' + t).padStart(2, '0'))
+      .join('-');
+
+    const fileString = this.logName + '-' + timeString;
+
+    return this.getLogPath(fileString);
+  }
+
+  private getLogPath(logName = this.logName) {
+    return resolve(root, join(LOGS_DIR, logName + LOG_EXTENSION));
   }
 }
